20ac30eadf15c9d326edd84b8525d7fe9a0ce9f4
歐家豪 <s105321019@wke.csie.ncnu.edu.tw>
2022-11-18 18:12:54 +0800

    add ResultFilter

---
+++ BaseAPI/Filters/ResultFilter.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace BaseAPI.Filters
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ResultFilter : IResultFilter
+    {
+        public void OnResultExecuted(ResultExecutedContext context)
+        {
+
+        }
+
+        public void OnResultExecuting(ResultExecutingContext context)
+        {
+            ObjectResult contextResult = context.Result as ObjectResult;
+            
+            switch (contextResult.StatusCode)
+            {
+                case (int)HttpStatusCode.OK:
+                    context.Result = GetOKResult(contextResult.Value, contextResult.StatusCode);                    
+                    break;                
+                default:
+                    context.Result = GetErrResult(contextResult.Value, contextResult.StatusCode);
+                    break;                    
+            }                         
+        }
+
+        private OkObjectResult GetOKResult(object? value, int? status_code)
+        {
+            var Result = new OkObjectResult(new {Data =  value });
+            Result.StatusCode = status_code;
+
+            return Result;
+        }
+
+        private ObjectResult GetErrResult(object? value, int? status_code)
+        {
+            var Result = new ObjectResult(new { ErrorMsg = value });
+            Result.StatusCode = status_code;
+
+            return Result;
+        }
+    }        
+}
--- BaseAPI/Program.cs
+++ BaseAPI/Program.cs
@@ -1,8 +1,15 @@
+using BaseAPI.Filters;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddMvc(options => {
+    options.Filters.Add(new ResultFilter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
